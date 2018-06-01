3ec7573d98f49627078e19a3505cd6b9ea11b158
Nick Rogers <RogersNL@live.com>
2018-06-01 15:22:28 -0700

    It will display an error if name is not entered and properly populate if new name is entered.

--- js/scripts.js
+++ js/scripts.js
@@ -4,91 +4,24 @@ $(document).ready(function(){
 
     // ASSIGN VARIABLES
     var userName = $("input#User-Name").val();
+    var userNameAgain = $("input#User-Name-Again").val();
     var inputQuestion1 = $("input:radio[name=size-company]:checked").val();
     var inputQuestion2 = $("input:radio[name=style-language]:checked").val();
     var inputQuestion3 = $("input:radio[name=discipline-preference]:checked").val();
     var inputQuestion4 = $("select#company-preference").val();
     var inputQuestion5 = $("input:radio[name=program-care]:checked").val();
 
-    // INSERT NAME
-    $(".Input-Name").text(userName);
-    // CHOOSING TRACK
-      // SMALL & FLEXIBLE
-    if (inputQuestion1 === "small" && inputQuestion2 === "flexible") {
-      if (inputQuestion5 === "logic") {
-        $("#CSharp-Info").hide();
-        $("#JAVA-Info").hide();
-        $("#PHP-Info").show();
-        $("#RUBY-Info").hide();
-      } else {
-        $("#CSharp-Info").hide();
-        $("#JAVA-Info").hide();
-        $("#PHP-Info").hide();
-        $("#RUBY-Info").show();
-      }
-
-      // LARGE & RULES
-    } else if (inputQuestion1 === "large" && inputQuestion2 === "rules") {
-      if (inputQuestion4 === "1") {
-        $("#CSharp-Info").show();
-        $("#JAVA-Info").hide();
-        $("#PHP-Info").hide();
-        $("#RUBY-Info").hide();
-      } else {
-        $("#CSharp-Info").hide();
-        $("#JAVA-Info").show();
-        $("#PHP-Info").hide();
-        $("#RUBY-Info").hide();
-      }
-
-      // Q1 AND Q2 CONFLICTING
+    // CHECK NAME
+    if (userName === "" && userNameAgain === "") {
+      $(".no-error").hide();
+      $(".has-error").show();
     } else {
-        // PHP TEST
-      if (inputQuestion3 === "scripts" && inputQuestion5 === "logic") {
-        $("#CSharp-Info").hide();
-        $("#JAVA-Info").hide();
-        $("#PHP-Info").show();
-        $("#RUBY-Info").hide();
-        //C# OR JAVA TEST
-      } else if (inputQuestion3 === "no-preference" && inputQuestion1 === "large") {
-        if (inputQuestion4 === "1") {
-          $("#CSharp-Info").show();
-          $("#JAVA-Info").hide();
-          $("#PHP-Info").hide();
-          $("#RUBY-Info").hide();
-        } else {
-          $("#CSharp-Info").hide();
-          $("#JAVA-Info").show();
-          $("#PHP-Info").hide();
-          $("#RUBY-Info").hide();
-        }
-
-        //RUBY OR JAVA TEST
-      } else if (inputQuestion3 === "app-creation") {
-        if (inputQuestion5 === "design") {
-          $("#CSharp-Info").hide();
-          $("#JAVA-Info").hide();
-          $("#PHP-Info").hide();
-          $("#RUBY-Info").show();
-        } else {
-          $("#CSharp-Info").hide();
-          $("#JAVA-Info").show();
-          $("#PHP-Info").hide();
-          $("#RUBY-Info").hide();
-        }
-        //OUTLIERS
-      } else {
-        if (inputQuestion4 === "1") {
-          $("#CSharp-Info").show();
-          $("#JAVA-Info").hide();
-          $("#PHP-Info").hide();
-          $("#RUBY-Info").hide();
-        } else if (inputQuestion4 === "2") {
-          $("#CSharp-Info").hide();
-          $("#JAVA-Info").show();
-          $("#PHP-Info").hide();
-          $("#RUBY-Info").hide();
-        } else if (inputQuestion4 === "3") {
+      // INSERT NAME
+      $(".Input-Name").text(userName + userNameAgain);
+      // CHOOSING TRACK
+        // SMALL & FLEXIBLE
+      if (inputQuestion1 === "small" && inputQuestion2 === "flexible") {
+        if (inputQuestion5 === "logic") {
           $("#CSharp-Info").hide();
           $("#JAVA-Info").hide();
           $("#PHP-Info").show();
@@ -98,6 +31,80 @@ $(document).ready(function(){
           $("#JAVA-Info").hide();
           $("#PHP-Info").hide();
           $("#RUBY-Info").show();
+        }
+
+        // LARGE & RULES
+      } else if (inputQuestion1 === "large" && inputQuestion2 === "rules") {
+        if (inputQuestion4 === "1") {
+          $("#CSharp-Info").show();
+          $("#JAVA-Info").hide();
+          $("#PHP-Info").hide();
+          $("#RUBY-Info").hide();
+        } else {
+          $("#CSharp-Info").hide();
+          $("#JAVA-Info").show();
+          $("#PHP-Info").hide();
+          $("#RUBY-Info").hide();
+        }
+
+        // Q1 AND Q2 CONFLICTING
+      } else {
+          // PHP TEST
+        if (inputQuestion3 === "scripts" && inputQuestion5 === "logic") {
+          $("#CSharp-Info").hide();
+          $("#JAVA-Info").hide();
+          $("#PHP-Info").show();
+          $("#RUBY-Info").hide();
+          //C# OR JAVA TEST
+        } else if (inputQuestion3 === "no-preference" && inputQuestion1 === "large") {
+          if (inputQuestion4 === "1") {
+            $("#CSharp-Info").show();
+            $("#JAVA-Info").hide();
+            $("#PHP-Info").hide();
+            $("#RUBY-Info").hide();
+          } else {
+            $("#CSharp-Info").hide();
+            $("#JAVA-Info").show();
+            $("#PHP-Info").hide();
+            $("#RUBY-Info").hide();
+          }
+
+          //RUBY OR JAVA TEST
+        } else if (inputQuestion3 === "app-creation") {
+          if (inputQuestion5 === "design") {
+            $("#CSharp-Info").hide();
+            $("#JAVA-Info").hide();
+            $("#PHP-Info").hide();
+            $("#RUBY-Info").show();
+          } else {
+            $("#CSharp-Info").hide();
+            $("#JAVA-Info").show();
+            $("#PHP-Info").hide();
+            $("#RUBY-Info").hide();
+          }
+          //OUTLIERS
+        } else {
+          if (inputQuestion4 === "1") {
+            $("#CSharp-Info").show();
+            $("#JAVA-Info").hide();
+            $("#PHP-Info").hide();
+            $("#RUBY-Info").hide();
+          } else if (inputQuestion4 === "2") {
+            $("#CSharp-Info").hide();
+            $("#JAVA-Info").show();
+            $("#PHP-Info").hide();
+            $("#RUBY-Info").hide();
+          } else if (inputQuestion4 === "3") {
+            $("#CSharp-Info").hide();
+            $("#JAVA-Info").hide();
+            $("#PHP-Info").show();
+            $("#RUBY-Info").hide();
+          } else {
+            $("#CSharp-Info").hide();
+            $("#JAVA-Info").hide();
+            $("#PHP-Info").hide();
+            $("#RUBY-Info").show();
+          }
         }
       }
     }
