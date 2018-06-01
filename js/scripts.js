899e7fb4d18569a590fd2c172f1f42c3368e36fe
Nick Rogers <RogersNL@live.com>
2018-06-01 13:30:35 -0700

    It will, through branching, select the most suitable language to start with using alerts.

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,62 @@ $(document).ready(function(){
   $('#Track-Form').submit(function(event){
     event.preventDefault();
 
+    // ASSIGN VARIABLES
     var inputQuestion1 = $("input:radio[name=size-company]:checked").val();
     var inputQuestion2 = $("input:radio[name=style-language]:checked").val();
     var inputQuestion3 = $("input:radio[name=discipline-preference]:checked").val();
-    var inputQuestion4 = ("select#company-preference").val();
+    var inputQuestion4 = $("select#company-preference").val();
     var inputQuestion5 = $("input:radio[name=program-care]:checked").val();
 
-    
+    // CHOOSING TRACK
+      // SMALL & FLEXIBLE
+    if (inputQuestion1 === "small" && inputQuestion2 === "flexible") {
+      if (inputQuestion5 === "logic") {
+        alert("PHP");
+      } else {
+        alert("RUBY");
+      }
+
+      // LARGE & RULES
+    } else if (inputQuestion1 === "large" && inputQuestion2 === "rules") {
+      if (inputQuestion4 === "1") {
+        alert("C#");
+      } else {
+        alert("JAVA");
+      }
+
+      // Q1 AND Q2 CONFLICTING
+    } else {
+        // PHP TEST
+      if (inputQuestion3 === "scripts" && inputQuestion5 === "logic") {
+        alert("PHP");
+        //C# OR JAVA TEST
+      } else if (inputQuestion3 === "no-preference" && inputQuestion1 === "large") {
+        if (inputQuestion4 === "1") {
+        alert("C#");
+        } else {
+        alert("JAVA");
+        }
+
+        //RUBY OR JAVA TEST
+      } else if (inputQuestion3 === "app-creation") {
+        if (inputQuestion5 === "design") {
+          alert("RUBY");
+        } else {
+          alert("JAVA");
+        }
+        //OUTLIERS
+      } else {
+        if (inputQuestion4 === "1") {
+          alert("C#");
+        } else if (inputQuestion4 === "2") {
+          alert("JAVA");
+        } else if (inputQuestion4 === "3") {
+          alert("PHP");
+        } else {
+          alert("RUBY");
+        }
+      }
+    }
   });
 });
