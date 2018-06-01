c188ea752d6f9d59aac149522f99935ab513e7a2
Nick Rogers <RogersNL@live.com>
2018-06-01 12:17:13 -0700

    It will display 5 questions and assign each answer to a variable in javascript.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
 $(document).ready(function(){
+  $('#Track-Form').submit(function(event){
+    event.preventDefault();
 
+    var inputQuestion1 = $("input:radio[name=size-company]:checked").val();
+    var inputQuestion2 = $("input:radio[name=style-language]:checked").val();
+    var inputQuestion3 = $("input:radio[name=discipline-preference]:checked").val();
+    var inputQuestion4 = ("select#company-preference").val();
+    var inputQuestion5 = $("input:radio[name=program-care]:checked").val();
 
+    
+  });
 });
